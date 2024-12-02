87077ed731b7041dec2bd704d21e31d12ed42fbe
Rohann-79 <124292835+Rohann-79@users.noreply.github.com>
2024-12-02 22:48:37 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,4 +1,3 @@
-# Importing Libraries
 import numpy as np
 from keras.models import Sequential
 from keras.preprocessing import sequence
@@ -7,41 +6,35 @@ from keras.datasets import imdb
 from keras.callbacks import EarlyStopping
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 import re
 import nltk
+from matplotlib import pyplot as plt
 
-# Download necessary NLTK data
 nltk.download('stopwords')
 nltk.download('wordnet')
 
-# Loading datasets
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=10000)
 word_index = imdb.get_word_index()
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
-# Preprocessing text
 def preprocess_text(text):
-    text = re.sub(r'<[^>]+>', '', text)  # Remove HTML tags
-    text = re.sub(r'\d+', '', text)  # Remove digits
-    text = re.sub(r'[^\w\s]', '', text)  # Remove special characters
-    text = text.lower()  # Convert to lowercase
-    stop_words = set(stopwords.words('english'))
+    text = re.sub(r'<[^>]+>', '', text)
+    text = re.sub(r'\d+', '', text)
+    text = re.sub(r'[^\w\s]', '', text)
+    text = text.lower()
+    stop_words = set(nltk.corpus.stopwords.words('english'))
     words = text.split()
     words = [word for word in words if word.lower() not in stop_words]
-    lemmatizer = WordNetLemmatizer()
+    lemmatizer = nltk.stem.WordNetLemmatizer()
     words = [lemmatizer.lemmatize(word) for word in words]
     return ' '.join(words)
 
-# Decoding reviews
 x_train_text = [' '.join([reverse_word_index.get(i - 3, '?') for i in sequence]) for sequence in x_train]
 x_test_text = [' '.join([reverse_word_index.get(i - 3, '?') for i in sequence]) for sequence in x_test]
 
 x_train_text = [preprocess_text(text) for text in x_train_text]
 x_test_text = [preprocess_text(text) for text in x_test_text]
 
-# Tokenize and Pad Sequence
 maxlen = 200
 tokenizer = Tokenizer(num_words=10000)
 tokenizer.fit_on_texts(x_train_text)
@@ -55,7 +48,6 @@ x_test = pad_sequences(x_test_seq, maxlen=maxlen)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
 
-# Model Building and Compiling
 n_unique_words = 10000
 model = Sequential()
 model.add(Embedding(n_unique_words, 64, input_length=maxlen))
@@ -65,11 +57,7 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Training the model
 history = model.fit(x_train, y_train, batch_size=128, epochs=10, validation_data=(x_test, y_test))
-
-# Plotting accuracy and loss
-from matplotlib import pyplot as plt
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
@@ -80,7 +68,6 @@ plt.xlabel('Epoch')
 plt.legend(['Loss', 'Accuracy', 'Val_Loss', 'Val_Accuracy'], loc='upper right')
 plt.show()
 
-# Making Predictions
 sample_text = "This is a great movie with fantastic performances!"
 sample_text = preprocess_text(sample_text)
 tokenized_sample = tokenizer.texts_to_sequences([sample_text])
